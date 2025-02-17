e25fab26bc48b34b62d05cc662fd9d545262f3ac
santiagobenedetti <sbenedetti@frba.utn.edu.ar>
2025-02-17 19:23:41 -0300

    test to see if deploy works

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,51 @@ import { Canvas } from "@react-three/fiber";
 import Room from "./components/Room";
 import { OrbitControls } from "@react-three/drei";
 import BaseLights from "./components/Lighting/BaseLights";
+
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          top: "20px",
+          left: "20px",
+          background: "rgba(72, 72, 94, 0.8)",
+          padding: "10px 20px",
+          borderRadius: "8px",
+          color: "white",
+          fontFamily: "Arial, sans-serif",
+          zIndex: 1000,
+          boxShadow: "0 2px 4px rgba(0,0,0,0.5)",
+        }}
+      >
+        <h1 style={{ fontSize: "24px", fontWeight: "bold" }}>3D Room Creator</h1>
+        <p style={{ fontSize: "14px" }}>
+          This is a 3D room creator to help you design your room.
+        </p>
+        <p style={{ fontSize: "14px" }}>
+          It allows you to explore a 3D model of a room in real-time and share it with your friends.
+        </p>
+      </div>
+
+      <div style={{
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        background: "rgba(38, 38, 49, 0.8)",
+        padding: "10px 20px",
+        borderRadius: "8px",
+        color: "white",
+        fontFamily: "Arial, sans-serif",
+        zIndex: 1000,
+        boxShadow: "0 2px 4px rgba(0,0,0,0.5)",
+      }}>
+        <p style={{ fontSize: "14px" }}>
+          Designed by <a href="https://github.com/santiagobenedetti" target="_blank" rel="noopener noreferrer">Santiago Benedetti</a>
+          and <a href="https://github.com/FranciscoAnnoni" target="_blank" rel="noopener noreferrer">Francisco Annoni</a>
+        </p>
+      </div>
+
       <Canvas shadows dpr={[1, 2]} camera={{ position: [22, 10, 30], fov: 45 }}>
         <color attach="background" args={["#20202a"]} />
         <BaseLights />
--- src/components/Room/index.tsx
+++ src/components/Room/index.tsx
@@ -48,13 +48,13 @@ const Room = () => {
       <Wall width={RIGHT_WALL_NEW_WIDTH} heigth={WALL_HEIGHT} depth={WALL_DEPTH} position={RIGHT_WALL_NEW_POSITION} rotation={RIGHT_WALL_ROTATION} />
 
       {/* <Model path='/models/desk/covered/escritorioConSilla.gltf' scale={1} position={[1.7, 0.3, 6]} rotation={[0, Math.PI / 2, 0]} /> */}
-      <Model path='/models/bed/covered/scene.gltf' scale={3.5} position={[10, 0.1, 3]} rotation={[0, Math.PI / 2, 0]} />
+      <Model path='models/bed/covered/scene.gltf' scale={3.5} position={[10, 0.1, 3]} rotation={[0, Math.PI / 2, 0]} />
       {/* <Model path='/models/plant/scene.gltf' scale={1} position={[1, 3.3, 7.3]} /> */}
-      <Model path='/models/coat_hanger/covered/scene.gltf' scale={1.4} position={[2, 0.1, 14]} rotation={[0, 1, 0]}/>
-      <Model path='/models/rug/scene.gltf' scale={0.04} position={[11, 0.27, 12]} />
+      <Model path='models/coat_hanger/covered/scene.gltf' scale={1.4} position={[2, 0.1, 14]} rotation={[0, 1, 0]}/>
+      <Model path='models/rug/scene.gltf' scale={0.04} position={[11, 0.27, 12]} />
 
-      <Model path='/models/construction_sign/untitled.gltf' scale={1} position={[21.5, 0.27, 15]} rotation={[0, -Math.PI / 2, 0]} />
-      <Model path='/models/sign/scene.gltf' scale={1} position={[13, 0.27, 15]} rotation={[0, -Math.PI / 2, 0]} />
+      <Model path='models/construction_sign/untitled.gltf' scale={1} position={[21.5, 0.27, 15]} rotation={[0, -Math.PI / 2, 0]} />
+      <Model path='models/sign/scene.gltf' scale={1} position={[13, 0.27, 15]} rotation={[0, -Math.PI / 2, 0]} />
 
       <Floor leftWallWidth={LEFT_WALL_WIDTH} rightWallWidth={RIGHT_WALL_WIDTH} depth={WALL_DEPTH}   position={FLOOR_POSITION} rotation={{ x: 0, y: 0, z: 0 }} />
     </>
